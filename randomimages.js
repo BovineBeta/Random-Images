7ec86a72f940e0b06a9062cbac154a9295d648ff
FBIK <83176414+BovineBeta@users.noreply.github.com>
2022-02-25 11:30:11 +0800

    Big fix

--- randomimages.js
+++ randomimages.js
@@ -1,16 +1,26 @@
-//设置随机函数
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+//让DOM加载完在加载本脚本
+window.onload=function(){
+    
+    //设置随机函数
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    //使用函数生成范围内随机数字
+    var num = getRndInteger(1,100);
+
+    //拼接后缀名
+    var path = num + ".png";
+
+    //生成链接
+    var url = "http://example.com/" + path;
+
+    //获取目标id
+    var rdi = document.getElementById("randomimages");
+
+    //修改DOM
+    rdi.src = url;
+
+    //如果只是想跳转, 则重定向至链接
+    //window.location.replace(url); 
 }
-
-//使用函数生成范围内随机数字
-var num = getRndInteger(1,100)
-
-//拼接后缀名
-var path = num + ".png"
-
-//生成链接
-var url = "http://example.com/" + path
-
-//重定向至链接
-window.location.replace(url)
