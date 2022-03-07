c6637dd7a6948cb0de52ba0aeca63a21510de042
FBIK <83176414+BovineBeta@users.noreply.github.com>
2022-03-07 15:21:41 +0800

    Update randomimages.easy.js

--- randomimages.easy.js
+++ randomimages.easy.js
@@ -1,1 +1,1 @@
-window.onload=function(){document.getElementById("randomimages").src = "http://example.com/"+ (Math.floor(Math.random() * (100 - 1)) + 1) + ".png";};
+window.onload=function(){document.getElementById("randomimages").src = "http://example.com/"+ (Math.floor(Math.random() * ((100 + 1) - 1)) + 1) + ".png";};
